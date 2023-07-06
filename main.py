88b6e389b80754fa4a46e5f688aa53f9b25565f6
Matteus Vargas Simao da Silva <matteus.silva@eldorado.org.br>
2023-07-06 16:06:44 -0300

    Alterações no código e no docker

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from mlflow.tracking import MlflowClient
 import pandas as pd
 import traci
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import networkx as nx
+
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -13,10 +16,9 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.ensemble import BaggingRegressor, AdaBoostRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, explained_variance_score
-import numpy as np
-import networkx as nx
-from concurrent.futures import ThreadPoolExecutor
+
+# Set up MLFlow tracking
+mlflow.set_tracking_uri('postgresql://mlflow:mlflow@db:5432/mlflow')
 
 # Constants
 BATCH_SIZE = 64
@@ -57,105 +59,77 @@ def validate_data(data):
 def train_models(X_train, y_train, metric, model_candidates):
     models = {}
     metrics = {}
-
     for vehicle_id, model in model_candidates.items():
         model.fit(X_train, y_train)
         models[vehicle_id] = model
-
+        y_pred = model.predict(X_train)
         if metric == 'min':
-            y_pred = model.predict(X_train)
             model_metric = mean_squared_error(y_train, y_pred)
         else:
-            y_pred = model.predict(X_train)
             model_metric = r2_score(y_train, y_pred)
-
         metrics[vehicle_id] = {
             'Metric': model_metric
         }
-
     return models, metrics
 
 
 def evaluate_models(models, X_test, y_test):
     performance_metrics = {}
-
     for vehicle_id, model in models.items():
         y_pred = model.predict(X_test)
-
         mse = mean_squared_error(y_test, y_pred)
-        mae = mean_absolute_error(y_test, y_pred)
         r2 = r2_score(y_test, y_pred)
-        evs = explained_variance_score(y_test, y_pred)
-
         performance_metrics[vehicle_id] = {
             'MSE': mse,
-            'MAE': mae,
-            'R2 Score': r2,
-            'Explained Variance Score': evs
+            'R2 Score': r2
         }
-
     return performance_metrics
 
 
 def select_alternate_routes(predictions, use_astar=False):
     graph = nx.DiGraph()
     available_routes = traci.route.getIDList()
-
     for route_id in available_routes:
         edges = traci.route.getEdges(route_id)
         for i in range(len(edges) - 1):
             source = edges[i]
             target = edges[i + 1]
             graph.add_edge(source, target)
-
     new_routes = {}
     shortest_paths = {}
-
     for vehicle_id, traffic_level_pred in predictions.items():
         current_route = traci.vehicle.getRoute(vehicle_id)
-
         if use_astar:
             shortest_paths = nx.single_source_astar_path(
                 graph, current_route[-1])
         else:
             shortest_paths = nx.single_source_dijkstra_path(
                 graph, current_route[-1])
-
         del shortest_paths[current_route[-1]]
         alternate_routes = list(shortest_paths.values())
-
         if not alternate_routes:
             new_routes[vehicle_id] = current_route
         else:
             best_route = min(alternate_routes,
                              key=lambda route: calculate_traffic(route))
             new_routes[vehicle_id] = best_route
-
     return new_routes, shortest_paths
 
 
 def calculate_traffic(route):
-    total_traffic = 0
-    for edge in route:
-        total_traffic += traci.edge.getLastStepMeanSpeed(edge)
-    return total_traffic
+    return sum(traci.edge.getLastStepMeanSpeed(edge) for edge in route)
 
 
 def evaluate_route_performance(new_routes, shortest_paths):
     performance_metrics = {}
-
     for vehicle_id, new_route in new_routes.items():
         old_route = traci.vehicle.getRoute(vehicle_id)
         old_travel_time = traci.vehicle.getAccumulatedWaitingTime(vehicle_id)
-
         traci.vehicle.setRoute(vehicle_id, new_route)
         traci.simulationStep()
         new_travel_time = traci.vehicle.getAccumulatedWaitingTime(vehicle_id)
-
         traci.vehicle.setRoute(vehicle_id, old_route)
-
         performance_metrics[vehicle_id] = old_travel_time - new_travel_time
-
     return performance_metrics, shortest_paths
 
 
@@ -189,7 +163,7 @@ def log_training_parameters():
 
 def log_evaluation_parameters():
     mlflow.log_param("Evaluation_Parameters", {
-        "Metrics": ["MSE", "MAE", "R2 Score"],
+        "Metrics": ["MSE", "R2 Score"],
         "Threshold": 0.5
     })
 
@@ -202,6 +176,7 @@ def log_configuration_parameters():
 
 
 def log_route_algorithm(use_astar):
+
     if use_astar:
         mlflow.log_param("Route_Algorithm", "A*")
     else:
@@ -224,76 +199,65 @@ def save_models(models):
         mlflow.log_artifact(model_path)
 
 
-def monitor_and_retrain(metric='min', use_astar=False, sumo_config_file="path/to/sumo_config_file.sumocfg", model_candidates=None):
+def monitor_and_retrain(metric='min', use_astar=False, sumo_config_file="due.actuated.sumocfg", model_candidates=None):
     client = MlflowClient()
     experiment_id = client.create_experiment("Federated Learning Experiment")
     run = client.create_run(experiment_id)
-
     data = collect_data(sumo_config_file)
     validate_data(data)
     X = data[['step', 'x', 'y']]
     y = data['traffic_level']
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42)
-
     models, train_metrics = train_models(
         X_train, y_train, metric, model_candidates)
-
     test_metrics = evaluate_models(models, X_test, y_test)
-
     with mlflow.start_run(run_id=run.info.run_id):
         log_metrics(train_metrics, prefix="Train")
         log_metrics(test_metrics, prefix="Test")
-
         log_model_parameters(models)
         log_preprocessing_parameters()
         log_training_parameters()
         log_evaluation_parameters()
         log_configuration_parameters()
-
         log_route_algorithm(use_astar)
-
-    client.set_terminated(run.info.run_id, "FINISHED")
-
+        client.set_terminated(run.info.run_id, "FINISHED")
     best_model_vehicle_id = min(test_metrics, key=test_metrics.get)
     best_model = models[best_model_vehicle_id]
-
     new_data = collect_data(sumo_config_file)
     new_routes, shortest_paths = select_alternate_routes(models, use_astar)
-
     performance_metrics, shortest_paths = evaluate_route_performance(
         new_routes, shortest_paths)
-
     for vehicle_id, metric_value in performance_metrics.items():
         test_metrics[vehicle_id]['Route_Performance'] = metric_value
-
     with mlflow.start_run(run_id=run.info.run_id):
         log_metrics(test_metrics, prefix="Test")
-
         log_best_model(best_model_vehicle_id)
         log_best_route(best_route_vehicle_id, best_route)
-
         save_models(models)
-
-    client.set_terminated(run.info.run_id, "FINISHED")
-
+        client.set_terminated(run.info.run_id, "FINISHED")
     best_route_vehicle_id = max(
         performance_metrics, key=performance_metrics.get)
     best_route = shortest_paths[best_route_vehicle_id]
-
     return best_model, best_route
 
 
 if __name__ == "__main__":
-    best_model, best_route = monitor_and_retrain(metric='min', use_astar=False, sumo_config_file="due.actuated.sumocfg", model_candidates={
+    # Define model candidates
+    model_candidates = {
         'Linear Regression': LinearRegression(),
         'Random Forest': RandomForestRegressor(),
         'Decision Tree': DecisionTreeRegressor(),
         'SVR': SVR(),
         'Gradient Boosting': GradientBoostingRegressor(),
-        'Neural Networks': MLPRegressor(),
+        'MLP': MLPRegressor(),
         'KNN': KNeighborsRegressor(),
         'Gaussian Process': GaussianProcessRegressor(),
         'Bagging': BaggingRegressor(),
         'AdaBoost': AdaBoostRegressor()
-    })
+
+    }
+    best_model, best_route = monitor_and_retrain(
+        metric='min', use_astar=False, sumo_config_file="due.actuated.sumocfg", model_candidates=model_candidates)
+    print(f"Best Model: {best_model}")
+    print(f"Best Route: {best_route}")
